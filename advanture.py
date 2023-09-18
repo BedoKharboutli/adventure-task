b3b9609da10305a6e6fab968b17afe4ab29bd038
bedo.kh <bedo.kharboutli@hotmail.com>
2023-09-18 15:09:44 +0200

    added functions

--- advanture.py
+++ advanture.py
@@ -1,23 +1,27 @@
-discription = "big room"
-doors = ["north", "south", "east", "west"]
+# Welcoming Screen Function.
+def WlcScreen():
+    print(
+        "Welcome to my first game in Python. Its a simple game that allows you to choose one of som options to make a movement "
+    )
+    print("__________________________________________")
+    print()
 
-# welcome screen
-print("Welcome to my first game in Python")
-print("__________________________________________")
-print()
-# mainloop
-run = True
-while run:
-    # print room
+
+# Description and the view of the room.
+def displayRoom(discription, doors):
     print("you are standing in" + " " + discription)
     print("And there are doors to your: ", doors)
-    # .
+    # Movements that are available in the room.
     directions = ""
     for directions in doors:
         directions = directions + ". " + directions
     directions = directions[2:]
     print(directions)
+    print()
 
+
+# Option menu for the player.
+def optionMenu():
     print("what do u want to do?")
     print("1. go north")
     print("2. go south")
@@ -26,7 +30,23 @@ while run:
     print("5. look")
     print("0. quit game")
 
-    # ask user for input
+
+discription = "big scary room "
+doors = ["north", "south", "east", "west"]
+
+# Welcoming Screen
+WlcScreen()
+
+# mainloop
+run = True
+while run:
+    # print the room for the player
+    displayRoom(discription, doors)
+
+    # showing the menu input for the player
+    optionMenu()
+
+    # ask player for input
     choice = input("please enter your choice : ")
 
     # santisize user input
@@ -39,15 +59,17 @@ while run:
         run = False
     elif choice == 1:
         print(
-            "you are going north, take a look at the nice view of the city from the window"
+            "You are going north, Good choice. That is the door that promises freedom ,take a deep breath and push the door open and step inside."
         )
     elif choice == 2:
-        print("you are going south")
+        print("you are going south ")
     elif choice == 3:
-        print("you are going east")
+        print(
+            "This door is locked, you need to get more damage points to be able to open this door"
+        )
     elif choice == 4:
-        print("you are going west")
+        print("Sorry, you reach a dead end, surrounded by walls closing in. Wasted")
     elif choice == 5:
         print("can not find anything new")
     else:
-        print("you asked for something i can no t do!")
+        print("you asked for something i can not do!")
